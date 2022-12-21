d534e7e8de2058975efb2b86e4ea852499e714c6
quannt86 <quannt86>
2022-12-21 13:57:33 +0700

    Add list page interface

--- src/utils/interfacePage.ts
+++ src/utils/interfacePage.ts
@@ -2,3 +2,12 @@ export interface IPage {
   permission: string[];
   isAllowed: (permission: string | string[]) => boolean;
 }
+
+export interface IListPage<T> {
+  [page: string]: {
+    Component: React.FC<T>;
+    allowedPosition: string[];
+    allowedWarehouse: string[];
+    allowedUsername: string[];
+  };
+}
